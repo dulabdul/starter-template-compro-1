644d1c3b856fac8aaf1583185c456d43b32598ce
dulabdul <dulabdol1331@gmail.com>
2026-02-21 15:24:29 +0700

    fix 404 not found in admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sanity from "@sanity/astro";
+// 1. Import dari '@astrojs/vercel' langsung
+import vercel from '@astrojs/vercel'; 
 
 export default defineConfig({
+  // 2. HAPUS opsi output: 'hybrid' sepenuhnya
+  
+  // 3. Adapter Vercel tetap di sini
+  adapter: vercel(),
+  
   integrations: [
     react(),
     sanity({
       projectId: "eh4q3bcq",
       dataset: "production",
-      // Mengaktifkan Studio di dalam Astro
       studioRoute: "/admin", 
     }),
   ],
