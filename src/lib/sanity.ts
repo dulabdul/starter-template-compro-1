0b1f3ad0ebefd60513eaf44fee269181688fb412
dulabdul <dulabdol1331@gmail.com>
2026-02-21 17:25:18 +0700

    fix data miss with sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,14 +1,17 @@
+// src/lib/sanity.ts
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const sanityClient = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  apiVersion: "2026-02-21", // use current date (YYYY-MM-DD) to target latest API
-  useCdn: true, // Set to false if you want fresh data on every fetch (slower)
+  apiVersion: "2024-03-01", 
+  
+  // WAJIB FALSE UNTUK ARSITEKTUR SSG + WEBHOOKS
+  // Ini memaksa Vercel melewati antrean Cache CDN dan langsung mengambil data terbaru dari Live API Sanity.
+  useCdn: false, 
 });
 
-// Helper for generating Sanity Image URLs
 const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(source: any) {
